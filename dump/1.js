440bdfd4d9133d69e18ddd2b8d75f3bda0e132ca
RazanTandan <rajantandan24@gmail.com>
2025-07-31 08:01:08 +0000

    Stop tracking dump directory and scores.db

--- dump/1.js
+++
@@ -1,9 +0,0 @@
-import { RpcProvider, constants } from 'starknet';
-
-const provider = new RpcProvider({ nodeUrl: constants.NetworkName.SN_SEPOLIA });
-
-const blockNumber = await provider.getBlockNumber(); 
-console.log(blockNumber);
-
-const chainId = await provider.getChainId();
-console.log(chainId);
